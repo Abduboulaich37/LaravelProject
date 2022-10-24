810b44702be4f43819abcc095929983388b51057
Abduboulaich37 <abduboulaich37@gmail.com>
2022-10-24 12:00:50 +0100

    insert , display done

--- PrototypeBasic/resources/views/index.blade.php
+++ PrototypeBasic/resources/views/index.blade.php
@@ -8,8 +8,20 @@
     <title>Document</title>
 </head>
 <body>
-    @foreach($variable as $key => $value)
-        
+<table>
+    <thead>
+        <th>id</th>
+        <th>name</th>
+        <th>action</th>
+    </thead>
+    <tbody>
+        @foreach($data as $promotions)
+        <tr>
+            <td>{{$promotions->id}}</td>
+            <td>{{$promotions->name}}</td>
+        </tr>
     @endforeach
+    </tbody>
+</table>
 </body>
 </html>
